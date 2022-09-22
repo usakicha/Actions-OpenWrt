878cbe4b9587483599724d6fba24de084cdf68dc
usakicha <liangkai20045@qq.com>
2022-09-23 02:45:49 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,7 +13,7 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
-wget -P ./target/linux/ipq40xx/patches-5.10/ https://raw.githubusercontent.com/usakicha/lede/master/target/linux/ipq40xx/patches-5.4/999-ipq40xx-unlock-cpu-frequency.patch
+wget -P ./target/linux/ipq40xx/patches-5.15/ https://raw.githubusercontent.com/usakicha/lede/master/target/linux/ipq40xx/patches-5.4/999-ipq40xx-unlock-cpu-frequency.patch
 echo "src-git-full luci2 https://github.com/coolsnowwolf/luci.git" >> ./feeds.conf.default
 ./scripts/feeds update luci2
 ./scripts/feeds install luci-app-cpufreq
